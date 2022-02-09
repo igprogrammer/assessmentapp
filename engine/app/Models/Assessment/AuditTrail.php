498a7ac818d4931e196c9d905979089a60b9f3b1
igprogrammer <igprogrammer88@gmail.com>
2022-02-09 14:44:33 +0300

    Assessment@090220221335PM

---
+++ engine/app/Models/Assessment/AuditTrail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Assessment;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AuditTrail extends Model
+{
+    use HasFactory;
+}
---
+++ engine/resources/views/assessment/fee_accounts/add_fee_account.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div style="border-bottom:none;padding-top:10px; padding-bottom:2px;background-color: white;margin-top: 10px;margin-left:3px;margin-right: 3px;margin-bottom: 0px" class="ask fbbluebox">
+
+
+
+        {!! Form::open(['url'=>'fees/save-fee-account','method'=>'post','class'=>'form','files'=>true])!!}
+
+        <div class="col-md-12">
+            <div class="panel panel-primary">
+                <!-- Default panel contents -->
+                <div class="panel-heading">Fee account details</div>
+                <div class="panel-body">
+                    @if(Session::has('success-message'))
+
+                        <div class="alert alert-success alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            {!! Session::get('success-message') !!}
+                        </div>
+                    @endif
+
+                    @if(Session::has('error-message'))
+
+                        <div class="alert alert-danger alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            {!! Session::get('error-message') !!}
+                            <p>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            </p>
+                        </div>
+                    @endif
+
+                    <div class="col-md-12">
+                        <div class="col-md-3">
+                            <div class="form-group {{$errors->has('questionTitle')?'has-error':''}}">
+                                {!! Form::label('title','Division') !!}
+                                {!! Form::select('division_id',[''=>'Select division']+$divisions,array(),['class'=>'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group {{$errors->has('questionTitle')?'has-error':''}}">
+                                {!! Form::label('title','Account code') !!}
+                                {!! Form::text('account_code',null,['class'=>'form-control','placeholder'=>'Account code']) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group {{$errors->has('questionTitle')?'has-error':''}}">
+                                {!! Form::label('title','Account name') !!}
+                                {!! Form::text('account_name',null,['class'=>'form-control','placeholder'=>'Account name']) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group {{$errors->has('questionTitle')?'has-error':''}}">
+                                {!! Form::label('title','Group number') !!}
+                                {!! Form::text('group_number',null,['class'=>'form-control','placeholder'=>'Group number']) !!}
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            {!! Form::submit('Add fee account',['class'=>'btn btn-success']) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {!!Form::close()!!}
+
+    </div>
+
+
+
+@stop
